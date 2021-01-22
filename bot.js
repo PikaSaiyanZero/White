2bdf35facddf82a38867bafefc6e076f5ab8c252
PikaSaiyanZero <64663174+PikaSaiyanZero@users.noreply.github.com>
2021-01-21 23:21:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,23 +10,12 @@ client.on('ready', () => {
 
 });
 
- 
-
 client.on('message', message => {
-
-    if (message.content === 'Pika es marica') {
-
-        message.reply('sos pollo washin');
-        try {
-            message.user.kick();
-        } catch {
-            message.reply('te salvaste putito no te pude dar tu rico ban')
-        }
-        
-
-       }
-
-});
+    if (message.content.startsWith("White, say ")) {
+       message.delete(1000); //Supposed to delete message
+       message.channel.send(message.content.slice(11, message.content.length));
+    }
+ });
 
 client.on('message', message => {
 
@@ -38,12 +27,12 @@ client.on('message', message => {
 
 });
 
-client.on('message', message => { //this event is fired, whenever the bot sees a new message
+/*client.on('message', message => { //this event is fired, whenever the bot sees a new message
     if (message.isMemberMentioned(client.user)) { //we check, whether the bot is mentioned, client.user returns the user that the client is logged in as
        //this is where you put what you want to do now
        message.reply('Saludos, mi nombre es White. Soy un felino cibernético diseñado por el caído Dios Máquina para servir a los Siete Guardianes en la protección del universo. En los últimos años, he acompañado a Astro Newlight en su viaje, y en mi tiempo libre ayudo en un servidor de Discord que tienen en la Tierra.');
     }
- });
+ });*/
 
 // THIS  MUST  BE  THIS  WAY
 
