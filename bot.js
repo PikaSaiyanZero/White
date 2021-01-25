8e1598844ef8c26e55a9e681676bd97400b7766a
PikaSaiyanZero <64663174+PikaSaiyanZero@users.noreply.github.com>
2021-01-24 21:26:36 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => { //this event is fired, whenever the bot sees a new message
-    if (message.isMemberMentioned(client.user)) { //we check, whether the bot is mentioned, client.user returns the user that the client is logged in as
+    if (message.isMemberMentioned(client.user) && message.content.toLowerCase().includes('saluda')) { //we check, whether the bot is mentioned, client.user returns the user that the client is logged in as
        //this is where you put what you want to do now
        message.reply('Saludos, mi nombre es White. Soy un felino cibernético diseñado por el caído Dios Máquina para servir a los Siete Guardianes en la protección del universo. En los últimos años, he acompañado a Astro Newlight en su viaje, y en mi tiempo libre ayudo en un servidor de Discord que tienen en la Tierra.');
     }
